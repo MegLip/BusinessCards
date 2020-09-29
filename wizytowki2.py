d96b6527fd1855f9ed623770432588570f253784
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-09-30 00:49:15 +0200

    Update and debugging

--- wizytowki2.py
+++ wizytowki2.py
@@ -1,36 +1,49 @@
 from faker import Faker
 fake = Faker("pl_PL")
 
-class BaseContact():                                                             #1 def klasy BaseContact
+
+class BaseContact():             # 1 def klasy BaseContact
     def __init__(self, first_name, last_name, phone, email):
         self.first_name = first_name
         self.last_name = last_name
         self.phone = phone
         self.email = email
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name}, {self.phone}, {self.email}"     
+        return f"{self.first_name}{self.last_name}, {self.phone}, {self.email}"
+
     @property
     def label_length(self):
-        return sum([len(self.first_name), len(self.last_name), 1])
+        return len(f"{self.first_name} {self.last_name}")
+
     @property
     def contact_number(self):
         return self.phone
-    def contact(self):
-        return f"Wybieram numer: {self.contact_number} i dzwonię do {self.first_name} {self.last_name}"
 
-class BusinessContact(BaseContact):                                                             #2 def klasy BusinessContact
-    def __init__(self, first_name, last_name, phone, email, job, company, business_phone):
+    def contact(self):
+        return f"Wybieram numer: {self.contact_number}\
+             \ni dzwonię do {self.first_name} {self.last_name}"
+
+
+class BusinessContact(BaseContact):    # 2 def klasy BusinessContact
+    def __init__(self, first_name, last_name, phone,
+                 email, job, company, business_phone):
         super().__init__(first_name, last_name, phone, email)
         self.job = job
         self.company = company
-        self.business_phone = business_phone            
+        self.business_phone = business_phone
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name}, {self.phone}, {self.email}, {self.job}, {self.company}, {self.business_phone}"
+        return f"{self.first_name} {self.last_name}, {self.phone},\
+                {self.email}, {self.job}, {self.company},\
+                    {self.business_phone}"
+
     @property
     def contact_number(self):
         return self.business_phone
 
-def create_contacts(type_of_card, number):                              #3 def function create_contacts
+
+def create_contacts(type_of_card, number):     # 3 def function create_contacts
     card_list = []
     for i in range(number):
         if type_of_card == "base":
@@ -56,17 +69,19 @@ def create_contacts(type_of_card, number):                              #3 def f
             )
     return card_list
 
-if __name__ == "__main__":                                          #4 Choise type of card and number of copies
+
+if __name__ == "__main__":        # 4 Choise type of card and number of copies
     type_of_card = input("Wybierz typ (base lub business): ")
     number = int(input("Podaj liczbę kopii: "))
+
     if type_of_card == 'base':
-        print('\n',"Wizytówki base:",'\n',"***************")
+        print('\n', "Wizytówki base:", '\n', "***************")
         base_cards = create_contacts("base", number)
         for card in base_cards:
             print(card.contact())
             print("Długość imienia i nazwiska: ", card.label_length, '\n')
     elif type_of_card == 'business':
-        print('\n',"Wizytówki biznesowe:",'\n',"********************")
+        print('\n', "Wizytówki biznesowe:", '\n', "********************")
         business_cards = create_contacts("business", number)
         for card in business_cards:
             print(card.contact())
